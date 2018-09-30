01f12e8b408b0cc45818b29393c3394f59f17f77
AnoopAmbunhi <AnoopAmbunhi@github.com>
2018-09-30 17:15:31 +0400

    added app path

--- driver/src/main/java/AppiumDriver/DesiredCapabilityIOS.java
+++ driver/src/main/java/AppiumDriver/DesiredCapabilityIOS.java
@@ -10,14 +10,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DesiredCapabilityIOS {
-    public static IOSDriver<IOSElement> capabilities() throws MalformedURLException {
+
+    IOSDriver<IOSElement> driver;
+
+    public IOSDriver<IOSElement> capabilities() throws MalformedURLException {
         DesiredCapabilities desired = new DesiredCapabilities();
         desired.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone X");
         desired.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         desired.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
         desired.setCapability(MobileCapabilityType.APP, "/Users/anoopambunhi/Desktop/Tajawal.ipa");
 
-        IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"), desired);
+        driver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"), desired);
         return driver;
     }
 }
