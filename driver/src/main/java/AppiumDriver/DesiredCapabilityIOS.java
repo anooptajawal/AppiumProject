0e0eaac434c93cac72b6fe206b03bed22ac5a6e1
AnoopAmbunhi <AnoopAmbunhi@github.com>
2018-09-30 16:36:36 +0400

    add git ignore and desired for ios

---
+++ driver/src/main/java/AppiumDriver/DesiredCapabilityIOS.java
@@ -0,0 +1,23 @@
+package AppiumDriver;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DesiredCapabilityIOS {
+    public static IOSDriver<IOSElement> capabilities() throws MalformedURLException {
+        DesiredCapabilities desired = new DesiredCapabilities();
+        desired.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone X");
+        desired.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        desired.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        desired.setCapability(MobileCapabilityType.APP, "/Users/anoopambunhi/Desktop");
+
+        IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("http://127.0.0.1:4723/wd/hub"), desired);
+        return driver;
+    }
+}
